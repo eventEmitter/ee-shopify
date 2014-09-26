65a622ff4485a82c1bef91a4c912553ad5ffc968
Michael van der Weg <michael@joinbox.com>
2014-09-27 01:04:13 +0200

    added models & querybuilders, more extensive API definition

--- test/default.js
+++ test/default.js
@@ -5,15 +5,33 @@
 		, assert 		= require('assert')
 		, travis 		= require('ee-travis');
 
+	//process.argv.push('--debug-shopify');
+
+	var   Shop = require('../')
+		, shop;
 
 
-	var Something = require('../')
 
+	describe('A Shopify Shop', function() {
+		it('should not crash when instantiated', function() {
+			shop = new Shop({
+				  shop 		: 'pasito-stage'
+				, key 		: 'api key'
+				, secret 	: 'secret'
+				, token 	: 'b3e09347c18e1a1b7773e8d7256e2d8b'
+			});
 
+			log(shop);
+		});
 
-	describe('Something', function(){
-		it('should', function(){
+		it('should expose the shopify api', function() {
+			assert(shop.order);
+		});
 
-		});		
+		it('should let you build queries', function(done) {
+			shop.blog(234).article().find(function(err, data) {
+				log(err, data);
+			})
+		});
 	});
 	
